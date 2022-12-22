bc3a9e06fdf750da4686a3318f31bfe4a224e5e7
Toby56 <Toby56@pm.me>
2022-12-22 16:57:54 +1100

    Clean up code and add better error handling - Remove redundant data formatting code - Add handling of jinja render errors - Add error display page

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import logging
 from flask import Flask, render_template
 import urllib
 import json
-import calendar
 from datetime import date
 
 # Sucky FTP server XML data method
@@ -13,16 +12,10 @@ from datetime import date
 
 # API and data formatting stuff
 API_URLs = {
-    'observations': 'https://api.weather.bom.gov.au/v1/locations/r1r0z4/observations',
-    'forecast_daily': 'https://api.weather.bom.gov.au/v1/locations/r1r0z4/forecasts/daily',
-    # 'forecast_3-hourly': 'https://api.weather.bom.gov.au/v1/locations/r1r0z4/forecasts/3-hourly'
+    'obs': 'https://api.weather.bom.gov.au/v1/locations/r1r0z4/observations',
+    'daily': 'https://api.weather.bom.gov.au/v1/locations/r1r0z4/forecasts/daily',
+    'hourly': 'https://api.weather.bom.gov.au/v1/locations/r1r0z4/forecasts/hourly'
 }
-
-data_format = {
-    'current_temp': lambda d : d['observations']['temp'],
-    'today_is_raining': lambda d : True if d['forecast_daily'][0] == 1 else False
-}
-
 
 def load_data(URL):
     try:
@@ -31,31 +24,23 @@ def load_data(URL):
     except urllib.error.HTTPError as error:
         logging.error(f'Failed to load data from URL: {URL}', error)
 
-def format_data(data_name, data):
-    try:
-        return data_format[data_name](data)
-    except KeyError as error:
-        logging.error(f'Data not found: {data_name}', error)
-        return "???"
-
 # Flask app
 app = Flask(__name__)
 if not app.debug:
     logging.basicConfig(filename='debug.log', format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.DEBUG)
 
 @app.route('/')
-def hello():
+def index():
     # Load data
     data = {}
     for data_category in API_URLs:
         data[data_category] = load_data(API_URLs[data_category])
 
-    # Format data
-    formatted_data = {}
-    for data_name in data_format:
-        formatted_data[data_name] = format_data(data_name, data)
-
     today = date.today()
     weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
     # Render and load HTML template!
-    return render_template('index.html', data=data, today=today, weekdays=weekdays)
+    try:
+        return render_template('index.html', data=data, today=today, weekdays=weekdays)
+    except Exception as error:
+        logging.error(error)
+        return render_template('error.html', error=error)
